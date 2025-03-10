7c647543015f8facee884a849fbddf5227cbd95a
yoavlav <Yoav.Lavie@mobileye.com>
2025-03-10 04:39:59 +0200

    update

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -5,12 +5,22 @@ import time
 
 import requests  # Used for the pre-test ping
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service  # <-- ADD THIS
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+
+
+# Set Chrome options (optional)
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Optional: Headless mode for Jenkins
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-gpu")
+
+driver_path = ChromeDriverManager().install()
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 try:
     # Load config.json
